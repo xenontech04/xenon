8c15a6cea7de1cef1ef365ce2d4f338b09fcca83
xenontech04 <153359270+xenontech04@users.noreply.github.com>
2023-12-28 12:15:12 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,17 +31,17 @@ function scrollActive(){
         }
     })
 }
-window.addEventListener('scroll', scrollActive)
+// window.addEventListener('scroll', scrollActive)
 
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 700,
-    delay: 200,
-    reset: true
-});
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 700,
+//     delay: 200,
+//     reset: true
+// });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 250}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .card',{interval: 200}); 
+// sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+// sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 250}); 
+// sr.reveal('.home__social-icon',{ interval: 200}); 
+// sr.reveal('.skills__data, .work__img, .card',{interval: 200}); 
